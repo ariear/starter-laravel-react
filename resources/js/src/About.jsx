d60887fc1a362d419113a6cc0ed19d3df72d912d
arie <arieinjector@gmail.com>
2023-03-15 12:27:28 +0700

    upgrade to laravel 10

--- resources/js/src/About.jsx
+++ resources/js/src/About.jsx
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="container text-center mx-auto my-16 bg-blue-400 p-4 rounded-lg">
             <p className="font-medium text-lg mb-4">About</p>
-            <p>Ini adalah sebuah projek stater laravel 9 , Tailwind CSS , React Js dengan Vite</p>
+            <p>Ini adalah sebuah projek stater laravel 10 , Tailwind CSS , React Js dengan Vite</p>
         </div>
     )
 }
