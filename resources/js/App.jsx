9d8e43c17c19046e8535a6c64338558509f7f1fb
Arie Akbarull Ridho <91861324+ariear@users.noreply.github.com>
2023-03-16 21:17:40 +0700

    change route

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import About from './src/About';
 import Home from './src/Home';
 import Root from './src/Root';
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        {
-            path: "/",
-            element: <Home />
-        },
-        {
-            path: "about",
-            element: <About />
-        }
-      ]
-    }
-]);
-
-if(document.getElementById('root')){
-    createRoot(document.getElementById('root')).render(
-        <RouterProvider router={router} />
+const App = () => {
+    return (
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Root />}>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/about' element={<About />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
     )
+}
+ 
+if(document.getElementById('root')){
+    createRoot(document.getElementById('root')).render(<App />)
 }
